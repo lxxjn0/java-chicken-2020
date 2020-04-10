3c60725b4cfca6c33db19f39f7c175d98f71b310
lxxjn0 <ljy3510@gmail.com>
2020-04-10 15:10:04 +0900

    feat : Order 인스턴스에 메뉴를 추가하는 기능 구현

--- src/main/java/domain/order/Order.java
+++ src/main/java/domain/order/Order.java
@@ -2,19 +2,25 @@ package domain.order;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import domain.order.menu.Menu;
 
 public class Order {
 
-	private final List<Menu> order;
+	private final List<Menu> menus;
 
-	private Order(final List<Menu> order) {
-		this.order = order;
+	private Order(final List<Menu> menus) {
+		this.menus = menus;
 	}
 
 	public Order() {
 		this(new ArrayList<>());
+	}
+
+	public void addMenu(final Menu menu) {
+		Objects.requireNonNull(menu, "메뉴가 null입니다.");
+		menus.add(menu);
 	}
 
 }
--- src/test/java/domain/order/OrderTest.java
+++ src/test/java/domain/order/OrderTest.java
@@ -4,9 +4,23 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import domain.order.menu.Category;
+import domain.order.menu.Menu;
+
 class OrderTest {
+
 	@Test
 	void Order_EmptyOrder_GenerateInstance() {
 		assertThat(new Order()).isInstanceOf(Order.class);
 	}
+
+	@Test
+	void addMenu_Menu_AddMenuToOrder() {
+		final Menu menu = new Menu("후라이드", Category.CHICKEN, 16_000);
+		final Order order = new Order();
+		order.addMenu(menu);
+
+		assertThat(order).extracting("menus").asList().contains(menu);
+	}
+
 }
