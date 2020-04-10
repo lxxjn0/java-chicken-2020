26191f1ee275fe9a7ad920f9faf6f29ea9f1cc63
lxxjn0 <ljy3510@gmail.com>
2020-04-10 13:51:48 +0900

    feat : 메뉴 최대 수량을 99개로 제한하는 기능 구현
    - 메뉴 수량을 감싸는 MenuQuantity 인스턴스 구현.

---
+++ src/main/java/domain/MenuQuantity.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class MenuQuantity {
+	private static final int LOWER_QUANTITY_BOUND = 0;
+	private static final int UPPER_QUANTITY_BOUND = 99;
+
+	private final int menuQuantity;
+
+	public MenuQuantity(final int menuQuantity) {
+		validate(menuQuantity);
+		this.menuQuantity = menuQuantity;
+	}
+
+	private void validate(final int menuQuantity) {
+		if (menuQuantity < LOWER_QUANTITY_BOUND || menuQuantity > UPPER_QUANTITY_BOUND) {
+			throw new IllegalArgumentException("유효한 메뉴 수량이 아닙니다.");
+		}
+	}
+}
---
+++ src/test/java/domain/MenuQuantityTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenuQuantityTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = {0, 99})
+	void MenuQuantity_QuantityOfMenu_GenerateInstance(final int menuQuantity) {
+		assertThat(new MenuQuantity(menuQuantity)).isInstanceOf(MenuQuantity.class);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 100})
+	void validate_InvalidMenuQuantity_ExceptionThrown(final int menuQuantity) {
+		assertThatThrownBy(() -> new MenuQuantity(menuQuantity))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("유효한 메뉴 수량이 아닙니다.");
+	}
+
+}
